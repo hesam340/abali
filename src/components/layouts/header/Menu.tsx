9e658de0683896ff60bf6f398fb98d557ed08777
hesam khaki <hesamkhaki340@gmail.com>
2026-08-26 11:51:20 +0330

    feat(swiper-header):add swiperSlide in header with swiper lib

--- src/components/layouts/header/Menu.tsx
+++ src/components/layouts/header/Menu.tsx
@@ -1,7 +1,7 @@
 import Link from "next/link";
 import Image from "next/image";
 
-import { subMenuTitles } from "@/core/constants/menuConstants";
+import { subMenuTitles } from "@/core/constants/headerConstants";
 
 function Menu() {
   return (
--- src/core/constants/menuConstants.ts
+++
@@ -1,6 +0,0 @@
-export const subMenuTitles = [
-  { id: 1, title: "ویژگی ها" },
-  { id: 2, title: "آشنایی با ما" },
-  { id: 3, title: "خرید آنلاین" },
-  { id: 4, title: "نظرات شما" },
-];
